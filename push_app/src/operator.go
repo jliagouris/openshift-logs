3e667434f58c4a1d6e24eb476e3435a26724c06e
jingyusu <gar123su@gmail.com>
2022-06-06 13:05:47 -0400

    operator API & struct update

--- push_app/src/operator.go
+++ push_app/src/operator.go
@@ -1,3 +1,24 @@
-package src
+package main
 
-//Main file of log operator
+//Main file of log pushing operator
+
+import (
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"push_app/src/components"
+)
+
+type operator struct {
+	producers []components.Kafka_producer
+}
+
+//Main function of the operator
+func main() {
+	//TODO: Fill this
+
+}
+
+//Gets the configurations that allow the operator talk to the correct Kafka brokers
+func getKafkaConfigMap() []kafka.ConfigMap {
+	//TODO: Fill this
+	return nil
+}
