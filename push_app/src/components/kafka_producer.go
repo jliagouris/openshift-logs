958630b5b7b859962751a1c71f0e101affc150d6
jingyusu <gar123su@gmail.com>
2022-06-06 13:06:10 -0400

    kafka_producer API & struct update

--- push_app/src/components/kafka_producer.go
+++ push_app/src/components/kafka_producer.go
@@ -1,3 +1,25 @@
 package components
 
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"os"
+)
+
 //Producer that sends queried and preprocessed logs to Kafka broker
+type Kafka_producer struct {
+	producer *kafka.Producer
+}
+
+func make_kafka_producer(configMap *kafka.ConfigMap) *Kafka_producer {
+	p, err := kafka.NewProducer(configMap)
+
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s", err)
+		os.Exit(1)
+	}
+
+	kp := Kafka_producer{producer: p}
+
+	return &kp
+}
