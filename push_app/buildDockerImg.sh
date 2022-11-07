090d9b1968fd3d8fd3f2a75d2cf182f7f08745c3
jingyusu <gar123su@gmail.com>
2022-11-06 23:05:39 -0500

    ad hoc docker script

--- push_app/buildDockerImg.sh
+++ push_app/buildDockerImg.sh
@@ -4,8 +4,8 @@ docker build -t prometheus-app .
 
 echo 'docker build finished'
 
-docker tag prometheus-app jingyusu/prometheus-app:1.0.1
+docker tag prometheus-app jingyusu/adhoc-app:1.0.0
 
 docker login
 
-docker push jingyusu/prometheus-app:1.0.1
+docker push jingyusu/adhoc-app:1.0.0
