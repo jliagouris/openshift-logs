4051d20430497274cc5d2df139b5efa07e3ea845
Jingyu <gar123su@gmail.com>
2022-12-29 23:05:42 -0500

    env var finalize

--- push_app/buildDockerImgAppleM1.sh
+++ push_app/buildDockerImgAppleM1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Use docker build -t push_app-1 . if not on apple m1 chip
-docker buildx build --platform linux/amd64 -t push_app-1 .
+docker buildx build --platform linux/amd64 -t push_app .
 
 echo 'docker build finished'
 
-docker tag push_app-1 jingyusu/push_app-1:demo
+docker tag push_app jingyusu/push_app:latest
 
 docker login
 
-docker push jingyusu/push_app-1:demo
+docker push jingyusu/push_app:latest
