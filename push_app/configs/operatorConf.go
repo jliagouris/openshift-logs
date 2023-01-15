01f4e86602efe0b9e4c9e264982ec3170003373c
Jingyu <gar123su@gmail.com>
2023-01-15 02:53:23 -0800

    config update

--- push_app/configs/operatorConf.go
+++ push_app/configs/operatorConf.go
@@ -5,5 +5,5 @@ type OperatorConf struct {
 	ChanBufSize    int    `yaml:"chan_buf_size"`
 	DataSourceType string `yaml:"data_source_type"`
 	TimeInterval   string `yaml:"time_interval"`
-	ClientId       string `yaml:"client_id"`
+	ClientId       string
 }
