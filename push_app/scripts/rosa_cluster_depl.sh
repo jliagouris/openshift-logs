410d5bf81239f06b9299862da7225242c745d53a
Jingyu <gar123su@gmail.com>
2023-01-15 02:52:41 -0800

    reformat

--- push_app/scripts/rosa_cluster_depl.sh
+++ push_app/scripts/rosa_cluster_depl.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
+client_id=$1
+echo "creating rosa-client-$client_id"
 rosa create account-roles --mode auto --yes
-rosa create cluster --cluster-name rosa-client --sts --mode auto --yes
+rosa create cluster --cluster-name rosa-client-$client_id --sts --mode auto --yes
 
 # wait until cluster is successfully deployed
-cluster_info=`rosa describe cluster -c rosa-client`
+cluster_info=`rosa describe cluster -c rosa-client-$client_id`
 cluster_state=`python3 scripts/pythonScripts/parseClusterState.py $cluster_info`
 while [ "$cluster_state" != "ready" ]
 do
-    echo "cluster is $cluster_state, check after 5m"
+    echo "cluster $1 is $cluster_state, check after 5m"
     sleep 300
-    cluster_info=`rosa describe cluster -c rosa-client`
+    cluster_info=`rosa describe cluster -c rosa-client-$client_id`
     cluster_state=`python3 scripts/pythonScripts/parseClusterState.py $cluster_info`
 done
 
 echo "Cluster installation finished"
+echo 'wait 10 min for cluster to initialize'
+sleep 600
+rosa create admin --cluster=rosa-client-$client_id > createAdmin-rosa-$client_id.txt
+admin_login_cmd=`python3 scripts/pythonScripts/parseAdminLogin.py`
+echo $admin_login_cmd
+echo 'wait 15 min for id server to get ready'
+sleep 900
+admin_login_output=`$admin_login_cmd`
+echo $admin_login_output
 
-./rosa_cluster_setup.sh
+./scripts/rosa_cluster_setup.sh 'rosa' $client_id
 
--- push_app/scripts/rosa_cluster_setup.sh
+++ push_app/scripts/rosa_cluster_setup.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-echo 'wait 10 min for cluster to initialize'
-sleep 600
-rosa create admin --cluster=rosa-client > createAdmin.txt
-admin_login_cmd=`python3 pythonScripts/parseAdminLogin.py`
-echo $admin_login_cmd
-echo 'wait 15 min for id server to get ready'
-sleep 900
-admin_login_output=`$admin_login_cmd`
-echo $admin_login_output
+cluster_provider=$1
+client_id=$2
 echo "Create Sucrose project"
 oc new-project sucrose-app --description="Prometheus scrape app with secrecy: Sucrose"
 oc project sucrose-app
@@ -17,15 +10,17 @@ echo 'wait 1 min for cluster to initialize new role'
 sleep 60
 service_acc_info=`oc describe sa default`
 echo $service_acc_info
-token_name=`python3 pythonScripts/parseSA.py $service_acc_info`
+token_name=`python3 scripts/pythonScripts/parseSA.py $service_acc_info`
 echo $token_name
 secret_info=`oc describe secret $token_name`
-token=`python3 pythonScripts/parseToken.py $secret_info`
+token=`python3 scripts/pythonScripts/parseToken.py $secret_info`
 echo 'token:'
 echo $token
-echo $token > ../credentials/token.txt
+echo $token > credentials/token-$cluster_provider-$client_id.txt
 echo '\n'
 echo 'prometheus url:\n'
 prom_url=`oc -n openshift-monitoring get routes | grep prometheus-k8s-openshift-monitoring | awk '{print $2}'`
 echo $prom_url
-echo $prom_url > ../credentials/prom_url.txt
+echo $prom_url > credentials/prom_url-$cluster_provider-$client_id.txt
+
+./scripts/sucrose_install.sh
