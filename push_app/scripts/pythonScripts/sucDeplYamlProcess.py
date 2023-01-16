cc06aa0363de18208a6ca48cd511e55e84a1c60a
Jingyu <gar123su@gmail.com>
2023-01-15 23:08:54 -0800

    update path

--- push_app/scripts/pythonScripts/sucDeplYamlProcess.py
+++ push_app/scripts/pythonScripts/sucDeplYamlProcess.py
@@ -2,21 +2,26 @@ import yaml
 import os
 import sys
 
+URL_IDX, TOKEN_IDX, CLIENT_ID_IDX = 0, 1, 2
+
 print(os. getcwd())
 with open(sys.path[0] + "/../../openshift-deploy.yaml", "r") as yamlfile:
     ocDeplConfig = yaml.load(yamlfile, Loader=yaml.FullLoader)
     print("Read successful")
     yamlfile.close()
 deplTemplateSpecContainerEnvVar = ocDeplConfig['spec']['template']['spec']['containers'][0]['env']
-URL_IDX, TOKEN_IDX = 0, 1
-with open(sys.path[0] + "/../../credentials/prom_url.txt", "r") as urlfile:
+with open(sys.path[0] + "/../../credentials/prom_url-" + sys.argv[1] + "-" + str(sys.argv[2]) + ".txt", "r") as urlfile:
     prom_url = urlfile.read()
     urlfile.close()
 deplTemplateSpecContainerEnvVar[URL_IDX]['value'] = prom_url[:-1]
-with open(sys.path[0] + "/../../credentials/token.txt", "r") as tokenfile:
+
+with open(sys.path[0] + "/../../credentials/token-" + sys.argv[1] + "-" + str(sys.argv[2]) + ".txt", "r") as tokenfile:
     token = tokenfile.read()
     tokenfile.close()
 deplTemplateSpecContainerEnvVar[TOKEN_IDX]['value'] = token[:-1]
+
+deplTemplateSpecContainerEnvVar[CLIENT_ID_IDX]['value'] = sys.argv[2]
+
 with open(sys.path[0] + "/../../openshift-deploy.yaml", "w") as yamlfile:
     ocDeplConfig = yaml.dump(ocDeplConfig, yamlfile)
     print(ocDeplConfig)
